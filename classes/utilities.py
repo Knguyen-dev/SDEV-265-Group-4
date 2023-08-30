1ff97996c6f4930e804362eaee3967a592617c7f
Kevin Nguyen <KNguyenSky@gmail.com>
2023-08-30 10:35:27 -0400

    Updates with current pages and additions
    All pages with forms now have an option to clear the form. All form fields relating to passwords now have the option to toggle whether the password is hidden or not. Created page for editing accounts, deleting accounts, and changing passwords. Added utilites, which is a file that contains good utility functions for doing aggregate operations on entry widgets. Next is the editAvatarPage.

---
+++ classes/utilities.py
@@ -0,0 +1,35 @@
+# Accepts list of entry widgets and clears their input
+from tkinter import *
+import customtkinter as ctk
+
+## Clears entry widgets of any input
+def clearEntryWidgets(entryWidgets):
+    for entry in entryWidgets:
+        entry.delete(0, END)
+
+
+## Toggles whether or not an entry widget's input is hidden, accepts a checkbox
+# Useful for hiding password input field and whatnot
+def toggleHidden(entryWidget, checkVar):
+	# If it's checked, user wants to hide password
+	if checkVar.get() == "on":
+		entryWidget.configure(show="*")
+	else:
+		entryWidget.configure(show="")
+
+## strips the input of entry widget of trailing or leading whitespace
+def stripEntryWidgets(entryWidgets):
+	strippedWidgets = []
+	for entry in entryWidgets:
+		inputValue = entry.get().strip()
+		entry.delete(0, END)
+		entry.insert(0, inputValue)
+		strippedWidgets.append(entry)
+	return strippedWidgets
+
+## Checks if any one of the entry widgets in the list entryWidgets has no input
+def isEmptyEntryWidgets(entryWidgets):
+	for entry in entryWidgets:
+		if entry.get() == "":
+			return True
+	return False
